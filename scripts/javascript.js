32e38d3b42d0b8acf2435af1469a8904215958f3
J F <J F>
2016-01-20 16:37:44 -0800

    fixed javascript

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,11 +1,8 @@
-document.getElementById('main').innerHTML = 'Hello World!';
 
 function click() {
   var d = new Date();
   document.getElementById('date').innerHTML = d.toDateString();
   document.getElementById('time').innerHTML = d.toLocaleTimeString();
 }
-
-//document.getElementById("button").addEventListener("click", click);
 
 var repeater = setInterval(click, 20);

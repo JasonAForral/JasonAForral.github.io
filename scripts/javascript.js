6fc79f1540ada5e1d4421a7c69b5222631d137ec
J F <J F>
2016-01-22 02:25:05 -0800

    Added svg animation

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,8 +1,36 @@
+var svgContainer = d3.select('#demo').append('svg');
 
-function click() {
+svgContainer
+  .attr('width', 360)
+  .attr('height', 360);
+
+var circles = svgContainer.selectAll('circle')
+  .data([60, 180, 300])
+  .enter()
+  .append('circle');
+  
+var circleAttributes = circles
+  .attr('cx', function (d) {return d;})
+  .attr('cy', 180)
+  .attr('r', 45);
+
+var angle = 0;
+var lastD = Date.now();
+
+function animation() {
   var d = new Date();
+  var ms = d.getMilliseconds();
+  angle += (Date.now() - lastD) * 0.05;
+  if (angle > 360) {
+    angle -= 360;
+  }
+  lastD = Date.now();
   document.getElementById('date').innerHTML = d.toDateString();
   document.getElementById('time').innerHTML = d.toLocaleTimeString();
+  var radians = (angle / 180 * Math.PI);
+  circleAttributes.attr('cy', function (d) {
+    return Math.sin( d - radians) * 60 + 180;
+  });
 }
 
-var repeater = setInterval(click, 20);
+var repeater = setInterval(animation, 20);

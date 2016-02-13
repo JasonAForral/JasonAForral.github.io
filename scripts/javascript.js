77c0ab113c9686323662f80f1296b25115b16c8c
J F <J F>
2016-02-12 23:15:08 -0800

    Added sound

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,59 +1,61 @@
-var svgContainer = d3.select('#demo').append('svg');
+(function () {/*
+  var svgContainer = d3.select('#demo').append('svg');
 
-svgContainer
-  .attr('width', 360)
-  .attr('height', 180);
+  svgContainer
+    .attr('width', 360)
+    .attr('height', 180);
 
-var circles = svgContainer.selectAll('circle')
-  .data([60, 180, 300])
-  .enter()
-  .append('circle');
-  
-var circleAttributes = circles
-  .attr('cx', function (d) {return d;})
-  .attr('cy', 180)
-  .attr('r', 20);
+  var circles = svgContainer.selectAll('circle')
+    .data([60, 180, 300])
+    .enter()
+    .append('circle');
+    
+  var circleAttributes = circles
+    .attr('cx', function (d) {return d;})
+    .attr('cy', 180)
+    .attr('r', 20);
 
-var angle = 0;
-var lastD = Date.now();
+  var angle = 0;
+  var lastD = Date.now();
 
-function animation() {
-  var d = new Date();
-  angle += (Date.now() - lastD) * 0.05;
-  if (angle > 360) {
-    angle -= 360;
+  function animation() {
+    var d = new Date();
+    angle += (Date.now() - lastD) * 0.05;
+    if (angle > 360) {
+      angle -= 360;
+    }
+    lastD = Date.now();
+    
+    document.getElementById('time').innerHTML = d.toLocaleTimeString();
+    var radians = (angle / 180 * Math.PI);
+    circleAttributes.attr('cy', function (d) {
+      return Math.sin( d - radians) * 60 + 90;
+    });
   }
-  lastD = Date.now();
-  
-  document.getElementById('time').innerHTML = d.toLocaleTimeString();
-  var radians = (angle / 180 * Math.PI);
-  circleAttributes.attr('cy', function (d) {
-    return Math.sin( d - radians) * 60 + 90;
-  });
-}
 
 
-var repeater = setInterval(animation, 20);
+  var repeater = setInterval(animation, 20);
 
-function displayDate() {
-var now = new Date();
+  function displayDate() {
+  var now = new Date();
 
-var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+  var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
 
-var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+  var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
 
-var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+  var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
 
-function fourdigits(number)	{
-  return (number < 1000) ? number + 1900 : number;
-}
-return days[now.getDay()] + ", " +
-         months[now.getMonth()] + " " +
-         date + ", " +
-         (fourdigits(now.getYear())) ;
+  function fourdigits(number)	{
+    return (number < 1000) ? number + 1900 : number;
+  }
+  return days[now.getDay()] + ", " +
+          months[now.getMonth()] + " " +
+          date + ", " +
+          (fourdigits(now.getYear())) ;
 
-}
+  }
 
-document.getElementById('date').innerHTML = displayDate();
-
-$(document).foundation();
+  document.getElementById('date').innerHTML = displayDate();
+  */
+  $(document).foundation();
+})();

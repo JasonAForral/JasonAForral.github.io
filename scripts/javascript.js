cde89395f2e654963a75b18f538b82a23fb8a096
J F <J F>
2016-07-08 21:20:02 -0700

    Newly Copied from Codepen

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,6 +1,15 @@
-'use strict';
+$('body').scrollspy({target: ".navbar"})
 
-(function () {
-  let m = a => a
-  document.body.textContent = m('Hello')
-})();
+$(window).on('resize', function() {
+  var win = $(window).height();
+  $('.section').css('min-height', win+10);
+  //$('.section').height(win);
+  //$('section').css('padding-top', win/4);
+});
+
+$(window).on('load', function() {
+  var win = $(window).height();
+  $('.section').css('min-height', win+10);
+  //$('.section').height(win);
+  //$('section').css('padding-top', win/4);
+});

bdbbc70efeb246c31f792b22758d2bb6a39297b7
Jason A. Forral <jason.forral@gmail.com>
2016-10-28 13:40:00 -0700

    basic custom css

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -117,20 +117,20 @@
   populatePortfolio(portfolioData);
 
 
-  $('body').scrollspy({ target: ".navbar" })
+  //$('body').scrollspy({ target: ".navbar" })
 
-  $(window).on('resize', function () {
-    var win = $(window).height();
-    $('.section').css('min-height', win + 10);
-    //$('.section').height(win);
-    //$('section').css('padding-top', win/4);
-  });
+  // $(window).on('resize', function () {
+  //   var win = $(window).height();
+  //   //$('.section').css('min-height', win + 10);
+  //   //$('.section').height(win);
+  //   //$('section').css('padding-top', win/4);
+  // });
 
-  $(window).on('load', function () {
-    var win = $(window).height();
-    $('.section').css('min-height', win + 10);
-    //$('.section').height(win);
-    //$('section').css('padding-top', win/4);
-  });
+  // $(window).on('load', function () {
+  //   var win = $(window).height();
+  //   $('.section').css('min-height', win + 10);
+  //   //$('.section').height(win);
+  //   //$('section').css('padding-top', win/4);
+  // });
 
 })()
